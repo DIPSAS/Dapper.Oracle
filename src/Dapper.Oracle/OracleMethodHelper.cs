3e7343e6b9023cd555652dec837c8dd3009ddfb2
Erling Paulsen <epa@dips.no>
2018-06-08 14:18:58 +0200

    Using Expression instead of reflection to set and get Oracle Properties

--- src/Dapper.Oracle/OracleMethodHelper.cs
+++ src/Dapper.Oracle/OracleMethodHelper.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Data;
+using System.Linq.Expressions;
 using System.Reflection;
+using Dapper.Oracle.Expressions;
 
 namespace Dapper.Oracle
 {
+
+
+
     internal class OracleMethodHelper
     {
-        private static readonly ConcurrentDictionary<Type, OracleParameterProperties> CachedOracleTypes = new ConcurrentDictionary<Type, OracleParameterProperties>();
-        private static readonly ConcurrentDictionary<Type, CommandProperties> CachedOracleCommandProperties = new ConcurrentDictionary<Type, CommandProperties>();
+        private static readonly ConcurrentDictionary<Type, OracleParameterExpressions> CachedOracleTypes = new ConcurrentDictionary<Type, OracleParameterExpressions>();
+        private static readonly ConcurrentDictionary<Type, CommandExpressions> CachedOracleCommandProperties = new ConcurrentDictionary<Type, CommandExpressions>();
 
         public static void SetArrayBindCount(IDbCommand command, int arrayBindCount)
         {
@@ -34,37 +39,37 @@ namespace Dapper.Oracle
 
             var method = CachedOracleTypes.GetOrAdd(parameter.GetType(), GetOracleProperties);
 
-            if (oracleParameterInfo.DbType != null)
+            if (oracleParameterInfo.DbType.HasValue)
             {
-                object value = (int)oracleParameterInfo.DbType;
-                method.OraDbType.SetValue(parameter, value);
+                method.OraDbType.SetValue(parameter, oracleParameterInfo.DbType.Value);
             }
 
             if (oracleParameterInfo.IsNullable.HasValue)
             {
-                method.IsNullable.SetValue(parameter, oracleParameterInfo.IsNullable.Value);
+                method.IsNullable.SetValue(parameter,oracleParameterInfo.IsNullable.Value);                
             }
 
             if (oracleParameterInfo.Scale.HasValue)
             {
-                method.Scale.SetValue(parameter, oracleParameterInfo.Scale.Value);
+                parameter.Scale = oracleParameterInfo.Scale.Value;
             }
 
             if (oracleParameterInfo.Precision.HasValue)
             {
-                method.Precision.SetValue(parameter, oracleParameterInfo.Precision.Value);
+                parameter.Precision = oracleParameterInfo.Precision.Value;
             }
 
-            method.SourceVersion.SetValue(parameter, oracleParameterInfo.SourceVersion);
+            parameter.SourceVersion = oracleParameterInfo.SourceVersion;
+
 
             if (oracleParameterInfo.SourceColumn != null)
             {
-                method.SourceColumn.SetValue(parameter, oracleParameterInfo.SourceColumn);
+                parameter.SourceColumn = oracleParameterInfo.SourceColumn;
             }
 
             if (oracleParameterInfo.CollectionType != OracleMappingCollectionType.None)
             {
-                method.CollectionType.SetValue(parameter, Enum.Parse(method.OracleCollectionEnumType, oracleParameterInfo.CollectionType.ToString()));
+                method.CollectionType.SetValue(parameter, oracleParameterInfo.CollectionType);
             }
 
             if (oracleParameterInfo.ArrayBindSize != null)
@@ -73,7 +78,7 @@ namespace Dapper.Oracle
             }
         }
 
-        internal static  OracleDynamicParameters.OracleParameterInfo GetParameterInfo(IDbDataParameter parameter)
+        internal static OracleDynamicParameters.OracleParameterInfo GetParameterInfo(IDbDataParameter parameter)
         {
             var method = GetOracleProperties(parameter.GetType());
             var paramInfo = new OracleDynamicParameters.OracleParameterInfo();
@@ -81,106 +86,74 @@ namespace Dapper.Oracle
             paramInfo.SourceVersion = parameter.SourceVersion;
             paramInfo.Precision = parameter.Precision;
             paramInfo.Size = parameter.Size;
-            
-            paramInfo.DbType = (OracleMappingType)Enum.Parse(
-                    typeof(OracleMappingType),
-                    Enum.GetName(method.OracleDbEnumType, method.OraDbType.GetValue(parameter)));
-            paramInfo.ArrayBindSize = (int[])method.ArrayBindSize.GetValue(parameter);
-            paramInfo.CollectionType= (OracleMappingCollectionType)Enum.Parse(
-                    typeof(OracleMappingCollectionType),
-                    Enum.GetName(method.OracleCollectionEnumType, method.CollectionType.GetValue(parameter)));            
+
+            paramInfo.DbType = method.OraDbType.GetValue(parameter);
+            paramInfo.ArrayBindSize = method.ArrayBindSize.GetValue(parameter);
+            paramInfo.CollectionType = method.CollectionType.GetValue(parameter);
             paramInfo.ParameterDirection = parameter.Direction;
             paramInfo.IsNullable = parameter.IsNullable;
             paramInfo.Scale = parameter.Scale;
             paramInfo.SourceColumn = parameter.SourceColumn;
-            paramInfo.Status = (OracleParameterMappingStatus)Enum.Parse(
-                    typeof(OracleParameterMappingStatus),
-                    Enum.GetName(method.OracleStatusEnumType,method.Status.GetValue(parameter)));
+            paramInfo.Status = method.Status.GetValue(parameter);
             paramInfo.Value = parameter.Value;
 
             return paramInfo;
         }
-        
-        private static OracleParameterProperties GetOracleProperties(Type type)
+
+        private static OracleParameterExpressions GetOracleProperties(Type type)
         {
-            Assembly assembly = type.Assembly;
-            var result = new OracleParameterProperties()
-            {
-                OraDbType = type.GetProperty("OracleDbType"),
-                Size = type.GetProperty("Size"),
-                IsNullable = type.GetProperty("IsNullable"),
-                Precision = type.GetProperty("Precision"),
-                Scale = type.GetProperty("Scale"),
-                SourceColumn = type.GetProperty("SourceColumn"),
-                SourceVersion = type.GetProperty("SourceVersion"),
-                CollectionType = type.GetProperty("CollectionType"),
-                ArrayBindSize = type.GetProperty("ArrayBindSize"),
-                Status = type.GetProperty("Status")
-            };
-            switch (type.FullName)
-            {
-                case "Oracle.DataAccess.Client.OracleParameter":
-                    result.OracleDbEnumType = assembly.GetType("Oracle.DataAccess.Client.OracleDbType");
-                    result.OracleCollectionEnumType = assembly.GetType("Oracle.DataAccess.Client.OracleCollectionType");
-                    result.OracleStatusEnumType = assembly.GetType("Oracle.DataAccess.Client.OracleParameterStatus");
-                    break;
-
-                case "Oracle.ManagedDataAccess.Client.OracleParameter":
-                    result.OracleDbEnumType = assembly.GetType("Oracle.ManagedDataAccess.Client.OracleDbType");
-                    result.OracleCollectionEnumType = assembly.GetType("Oracle.ManagedDataAccess.Client.OracleCollectionType");
-                    result.OracleStatusEnumType = assembly.GetType("Oracle.ManagedDataAccess.Client.OracleParameterStatus");
-                    break;
-                default:
-                    throw new NotSupportedException(
-                        $"OracleDynamicParameters only works with Oracle objects, you cannot use this for parameter type {type.FullName}");
-            }
-
-            return result;
+            return new OracleParameterExpressions(type);
         }
 
-        private static CommandProperties Get(IDbCommand command)
+        private static CommandExpressions Get(IDbCommand command)
         {
             return CachedOracleCommandProperties.GetOrAdd(command.GetType(), type =>
             {
-                var c = new CommandProperties
-                {
-                    ArrayBindCount = type.GetProperty("ArrayBindCount"),
-                    InitialLOBFetchSize = type.GetProperty("InitialLOBFetchSize"),
-                    BindByName = type.GetProperty("BindByName")
-                };
-                return c;
+                return new CommandExpressions(type);
             });
         }
 
-
-        private class OracleParameterProperties
+        private class OracleParameterExpressions
         {
-            public PropertyInfo OraDbType { get; set; }
+            public OracleParameterExpressions(Type oracleParameterType)
+            {
+                if (!oracleParameterType.Namespace.StartsWith("Oracle"))
+                {
+                    throw new NotSupportedException($"Whoopsies! This library will only work with Oracle types, you are attempting to use type {oracleParameterType.FullName}.");
+                }
 
-            public PropertyInfo Size { get; set; }
+                OraDbType = new ObjectEnumWrapper<IDbDataParameter, OracleMappingType>("OracleDbType", "OracleDbType", oracleParameterType);
+                ArrayBindSize = new ObjectWrapper<IDbDataParameter, int[]>("ArrayBindSize", oracleParameterType);
+                CollectionType = new ObjectEnumWrapper<IDbDataParameter, OracleMappingCollectionType>("OracleCollectionType", "CollectionType", oracleParameterType);
+                Status = new ObjectEnumWrapper<IDbDataParameter, OracleParameterMappingStatus>("Status", "Status", oracleParameterType);
+                IsNullable = new ObjectWrapper<IDbDataParameter, bool>("IsNullable",oracleParameterType);
+            }
 
-            public PropertyInfo IsNullable { get; set; }
+            public ObjectEnumWrapper<IDbDataParameter, OracleMappingType> OraDbType { get; }
 
-            public PropertyInfo Precision { get; set; }
+            public ObjectWrapper<IDbDataParameter, int[]> ArrayBindSize { get; }
 
-            public PropertyInfo Scale { get; set; }
+            public ObjectWrapper<IDbDataParameter, bool> IsNullable { get; }
 
-            public PropertyInfo ArrayBindSize { get; set; }
+            public ObjectEnumWrapper<IDbDataParameter, OracleMappingCollectionType> CollectionType { get; }
 
-            public PropertyInfo SourceColumn { get; set; }
+            public ObjectEnumWrapper<IDbDataParameter, OracleParameterMappingStatus> Status { get; }
+        }
 
-            public PropertyInfo SourceVersion { get; set; }
+        private class CommandExpressions
+        {
+            public ObjectWrapper<IDbCommand, bool> BindByName { get; }
+            public ObjectWrapper<IDbCommand, int> InitialLOBFetchSize { get; }
+            public ObjectWrapper<IDbCommand, int> ArrayBindCount { get; }
 
-            public PropertyInfo CollectionType { get; set; }
+            public CommandExpressions(Type commandType)
+            {
+                BindByName = new ObjectWrapper<IDbCommand, bool>("BindByName", commandType);
+                InitialLOBFetchSize = new ObjectWrapper<IDbCommand, int>("InitialLOBFetchSize", commandType);
+                ArrayBindCount = new ObjectWrapper<IDbCommand, int>("ArrayBindCount", commandType);
+            }
 
-            public PropertyInfo Status { get; set; }
 
-            public Type OracleDbEnumType { get; set; }
-
-            public Type OracleStatusEnumType { get; set; }
-
-            public Type OracleCollectionEnumType { get; set; }
-            
         }
 
         private class CommandProperties
---
+++ src/Tests.Dapper.Oracle/Expressions/ObjectWrapperTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper.Oracle;
+using Dapper.Oracle.Expressions;
+using FluentAssertions;
+using Xunit;
+
+using Managed = Oracle.ManagedDataAccess.Client;
+#if NET452
+using UnManaged = Oracle.DataAccess.Client;
+#endif
+
+namespace Tests.Dapper.Oracle.Expressions
+{
+    public class ParameterBaseTests
+    {
+        public static IEnumerable<object[]> OracleDataFixture
+        {
+            get
+            {
+#if NETCOREAPP2_0
+                yield return new object[] {new Managed.OracleCommand()};
+#else
+                yield return new object[] { new Managed.OracleCommand() };
+                yield return new object[] { new UnManaged.OracleCommand() };
+#endif                                                
+            }
+        }
+    }
+
+    public class ObjectEnumWrapperTests : ParameterBaseTests
+    {
+        [Theory, MemberData(nameof(OracleDataFixture))]
+        public void BasicTest(IDbCommand cmd)
+        {
+            var param = cmd.CreateParameter();
+
+            var setter = new ObjectEnumWrapper<IDbDataParameter, OracleMappingType>("OracleDbType", "OracleDbType", param.GetType());
+            setter.SetValue(param, OracleMappingType.Date);
+            var value = setter.GetValue(param);
+            value.ToString().Should().Be("Date");
+        }        
+    }
+
+    public class ObjectWrapperTests : ParameterBaseTests
+    {
+        [Theory, MemberData(nameof(OracleDataFixture))]
+        public void Test(IDbCommand cmd)
+        {
+            var param = cmd.CreateParameter();
+            var wrapper = new ObjectWrapper<IDbDataParameter, int>("Size", param.GetType());
+            wrapper.SetValue(param,100);
+            var result = wrapper.GetValue(param);
+            result.Should().Be(100);
+        }
+    }
+}
